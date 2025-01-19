4ad2d0f2cbaf7b048ec6fb04ce89dae42bd0c2b3
Ahmed Roberts <aroberts@crossroadshealth.org>
2025-01-18 22:45:24 -0700

    :hear_no_evil: :see_no_evil: :speak_no_evil:

---
+++ raikage_script.js
@@ -0,0 +1,21 @@
+// Displaying an alert message
+alert("Hello, World!");
+
+// Writing to the browser console
+console.log("This is a console message.");
+
+// Creating a variable and displaying it
+let message = "Hello, JavaScript!";
+console.log(message);
+
+// Performing a calculation
+let x = 5;
+let y = 10;
+let sum = x + y;
+console.log("The sum is: " + sum);
+
+// Using a function
+function greet(name) {
+  console.log("Hello, " + name + "!");
+}
+greet("John");
